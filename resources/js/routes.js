6373be0c8209838172baaa88124e0ba898c9bf4a
Daniel <angelicodaniel93@gmail.com>
2023-02-03 16:48:05 +0100

    /tags

--- resources/js/routes.js
+++ resources/js/routes.js
@@ -11,6 +11,8 @@ import HomePage from './views/pages/HomePage.vue'
 import PostsIndex from './views/pages/posts/PostsIndex.vue'
 import PostShow from './views/pages/posts/PostShow.vue'
 
+import TagsIndex from './views/pages/tags/TagsIndex.vue'
+
 const router = new VueRouter({
     //paths
     mode: 'history',
@@ -34,6 +36,11 @@ const router = new VueRouter({
             path: '/posts/:id',
             name: 'singlePost',
             component: PostShow
+        },
+        {
+            path: '/tags',
+            name: 'tags',
+            component: TagsIndex
         }
     ]
 });
